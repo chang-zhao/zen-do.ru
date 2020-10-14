d082b87ba8ef4dc3c3eab9f45366a56e27f14bc2
Chang Zhao <chang-zhao@users.noreply.github.com>
2020-10-14 22:09:41 +0300

    Update wrap.sh

--- wrap.sh
+++ wrap.sh
@@ -11,4 +11,8 @@ find ./in -name "*" | while read i;
 do
     echo "${i/in/out}";
     cat ./tpl/header.html "$i" ./tpl/footer.html >"${i/in/out}"
+    git add "${i/in/out}"
 done
+git config user.name "chang-zhao"
+git commit . -m "Wrapping"
+git push origin main
